f7a750e6825e5d234652c980b62105b4cfd06238
Aron <aron.mw12@gmail.com>
2023-10-04 11:27:03 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Stapel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testen met integers
+        Stapel<int> intStack = new Stapel<int>();
+        intStack.Opzetten(1);
+        intStack.Opzetten(2);
+        intStack.Opzetten(3);
+        Console.WriteLine("Integer Stapel:");
+        Console.WriteLine(intStack);
+        int item1 = intStack.Afhalen();
+        Console.WriteLine($"Afgehaald: {item1}");
+        Console.WriteLine(intStack);
+        intStack.Leegmaken();
+        Console.WriteLine("Na leegmaken:");
+        Console.WriteLine(intStack);
+
+
+        // Testen met een zelfgemaakte klasse (bijvoorbeeld een eenvoudige Point-klasse)
+//        Stapel<Point> pointStack = new Stapel<Point>();
+//        pointStack.Opzetten(new Point(1, 2));
+//        pointStack.Opzetten(new Point(3, 4));
+//        pointStack.Opzetten(new Point(5, 6));
+//        Console.WriteLine("\nPoint Stapel:");
+//        Console.WriteLine(pointStack);
+//        Point item3 = pointStack.Afhalen();
+//        Console.WriteLine($"Afgehaald: {item3}");
+//        Console.WriteLine(pointStack);
+//        pointStack.Leegmaken();
+//        Console.WriteLine("Na leegmaken:");
+//        Console.WriteLine(pointStack);
+//    }
+//}
+
+//public class Point
+//{
+//    public int X { get; set; }
+//    public int Y { get; set; }
+
+//    public Point(int x, int y)
+//    {
+//        X = x;
+//        Y = y;
+//    }
+
+//    public override string ToString()
+//    {
+//        return $"({X}, {Y})";
+//    }
+}
+
---
+++ Stapel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stapel
+{
+
+    public class Stapel<T>
+    {
+        private List<T> items;
+
+        public Stapel()
+        {
+            items = new List<T>();
+        }
+
+        public void Opzetten(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Afhalen()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("De stapel is leeg.");
+            }
+
+            T item = items[items.Count - 1];
+            items.RemoveAt(items.Count - 1);
+            return item;
+        }
+
+        public void Leegmaken()
+        {
+            items.Clear();
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = items.Count - 1; i >= 0; i--)
+            {
+                string indent = new string(' ', items.Count - 1 - i);
+                result += $"{indent}{items[i]}\n";
+            }
+            return result;
+        }
+    }
+}
